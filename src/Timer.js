977e102450dac0ade8c0e63a0b1b2d741ae27d02
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-04-12 00:26:56 -0500

    added Timer class

---
+++ src/Timer.js
@@ -0,0 +1,10 @@
+class Timer {
+    // The name property is passed into the constructor and the class is initialized.
+    constructor(name) {
+      this.name = name;
+      this.time = 0;
+      this.isRunning = false;
+    }
+  }
+  
+  export default Timer;
