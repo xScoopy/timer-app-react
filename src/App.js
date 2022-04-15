51c265a5d4ae23d9117fa259f7678961123ba728
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-04-14 21:46:31 -0500

    added timer reducers adn imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
 
 function App() {
+  const store = createStore(reducers);
   return (
     <div className="App">
+      <Provider store={store}>
+        <h1>TMRZ</h1>
+      </Provider>
     </div>
   );
 }
